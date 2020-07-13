071d791ddf404dbf65748e92f65b0874204f423a
Timo Engelvaart <timoengelvaart@Timo-2.local>
2020-07-13 19:50:14 +0200

    second

--- push.js
+++ push.js
@@ -7,6 +7,6 @@ let vapIdKeys = push.generateVAPIDKeys = {
 
 push.setVapidDetails('mailto:test@code.co.uk', vapIdKeys.publicKey, vapIdKeys.privateKey)
 
-let sub={"endpoint":"https://fcm.googleapis.com/fcm/send/fa5LJhA0Ypg:APA91bE1lcZW096anFgJtPDBWQNx6CceHuvrHmnF4N02uYQPOtV5iVRQSCr9r_URYpA4Pn4lscAMK6aggNnlMUUBv1d37AdxjQy3pqb6MliOlh03ZyOzVegv8yUiQeZ7_H39cwY0Xr6B","expirationTime":null,"keys":{"p256dh":"BKP162-bETnsjyxQa87hQbjodmK4ZSexcYwS_ELsr_hDDi8zL5WTt6NKuO_f_va1YT31G98fdYU-56tHig3vfIc","auth":"vnmOqXBW0xzWhvNJVIVWMQ"}};
+let sub={"endpoint":"https://fcm.googleapis.com/fcm/send/fa5LJhA0Ypg:APA91bE1lcZW096anFgJtPDBWQNx6CceHuvrHmnF4N02uYQPOtV5iVRQSCr9r_URYpA4Pn4lscAMK6aggNnlMUUBv1d37AdxjQy3pqb6MliOlh03ZyOzVegv8yUiQeZ7_H39cwY0Xr6B","expirationTime":null,"keys":{"p256dh":"BKP162-bETnsjyxQa87hQbjodmK4ZSexcYwS_ELsr_hDDi8zL5WTt6NKuO_f_va1YT31G98fdYU-56tHig3vfIc","auth":"vnmOqXBW0xzWhvNJVIVWMQ"}}
 
 push.sendNotification(sub, 'test message');
